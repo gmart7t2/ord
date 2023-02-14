b72401187f50981b7d1e30ac96b32d263b7c4636
Andrew Toth <andrewstoth@gmail.com>
2023-02-14 11:52:58 -0500

    Add retry for fetching raw transaction

--- src/index/rest.rs
+++ src/index/rest.rs
@@ -1,9 +1,13 @@
 use {
-  anyhow::Result,
+  anyhow::{anyhow, Result},
   bitcoin::{consensus::deserialize, BlockHash, Transaction, Txid},
   hyper::{client::HttpConnector, Client, Uri},
-  std::str::FromStr,
+  std::{str::FromStr, time::Duration},
+  tokio::time::sleep,
 };
+
+const MAX_CONNECTIONS: usize = 100;
+const RETRY_COUNT: usize = 3;
 
 pub(crate) struct Rest {
   client: Client<HttpConnector>,
@@ -17,10 +21,10 @@ impl Rest {
     } else {
       url.to_string()
     };
-    Rest {
-      client: Client::new(),
-      url,
-    }
+    let client = Client::builder()
+      .pool_max_idle_per_host(MAX_CONNECTIONS)
+      .build_http();
+    Rest { client, url }
   }
 
   pub(crate) async fn get_block_hash(&self, height: u32) -> Result<BlockHash> {
@@ -32,10 +36,25 @@ impl Rest {
   }
 
   pub(crate) async fn get_raw_transaction(&self, txid: &Txid) -> Result<Transaction> {
-    let url = format!("{}/rest/tx/{txid:x}.bin", self.url);
-    let res = self.client.get(Uri::from_str(&url)?).await?;
-    let buf = hyper::body::to_bytes(res).await?;
-    let tx = deserialize(&buf)?;
-    Ok(tx)
+    for i in 0..RETRY_COUNT {
+      let res: Result<Transaction> = async {
+        let url = format!("{}/rest/tx/{txid:x}.bin", self.url);
+        let res = self.client.get(Uri::from_str(&url)?).await?;
+        let buf = hyper::body::to_bytes(res).await?;
+        let tx = deserialize(&buf)?;
+        Ok(tx)
+      }
+      .await;
+      match res {
+        Ok(res) => {
+          return Ok(res);
+        }
+        Err(_) => {
+          let duration = 2 ^ i;
+          sleep(Duration::from_secs(duration as u64)).await;
+        }
+      };
+    }
+    return Err(anyhow!("Could not fetch tx {txid:x}"));
   }
 }
